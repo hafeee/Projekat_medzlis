5437b273ca1b91f086fa879229f70625923ea57e
Hamza <ieri.hamza@gmail.com>
2017-08-30 14:21:29 +0200

    ok

---
+++ New folder/test/eksten/Program2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public static class Klasica
+    {
+        public static string fun(this string str)
+        {
+            str = "Hamza";
+            return str;
+        }
+
+    }
+}
---
+++ New folder/test/test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; //zbog ovoga se nad int[] moze pozvati funkcija Orderby koju implementira IEnumerable<T>
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 10, 45, 15, 39, 21, 26 };
+            var result = ints.OrderBy(g => g);
+            foreach (var i in result)
+            {
+                System.Console.Write(i + " ");
+            }
+            string k = "Muho";
+            System.Console.Write(k.fun());
+        }
+    }
+}
